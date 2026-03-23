a66c130c0cc7be3d1d8b3780cb1386d09700dac2
Potato <yamasjto@gmail.com>
2026-03-23 23:53:25 +0900

    feat: 作った

--- repository/task.go
+++ repository/task.go
@@ -10,3 +10,11 @@ type Task struct {
 	Title  string
 	Status string
 }
+
+type TaskRepository struct {
+	db *gorm.DB
+}
+
+func (r *TaskRepository) Create(task *Task) error {
+	return r.db.Create(task).Error
+}
