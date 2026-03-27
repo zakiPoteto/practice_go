a9f011645489d38ca26c54903950c5d1fb65420e
Souma Yamazaki <yamas.jto@gmail.com>
2026-03-27 18:06:37 +0900

    feat: DB初期化

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
+	// "practice_go/handler"
+	"practice_go/task"
 
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+	"gorm.io/gorm"
+)
 func main() {
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello, World!")
 	})
+	
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("failed to connect database:", err)
+		return
+	}
+	db.AutoMigrate(&task.Task{})
 
 	r.Run(":8080")
 
