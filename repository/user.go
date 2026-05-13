461c7b7f20461407c7b6c2c75c28f01ab4a109f3
zakiPoteto <yamas.jto@gmail.com>
2026-05-13 14:16:14 +0900

    feat: UserRepositoryの実装

---
+++ repository/user.go
@@ -0,0 +1,25 @@
+package repository
+
+import (
+	"todo-api/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+func (r *UserRepository) Create(user *model.User) error {
+	return r.db.Create(user).Error
+}
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	var user model.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
